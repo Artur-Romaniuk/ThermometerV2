a37ae49d8ad37635454a6b5536b8f696be86a84f
Artur R <artur.secure@protonmail.ch>
2021-06-23 16:14:22 +0200

    Defined timer handles for DS18B20.h file

--- Core/Inc/DS18B20.h
+++ Core/Inc/DS18B20.h
@@ -8,29 +8,26 @@
 #define DS18B20_Port DS18B20_DQ_GPIO_Port
 #define DS18B20_Pin DS18B20_DQ_Pin
 
+#define MICRO_DELAY_TIM_HANDLE &htim6
+#define STATE_MACHINE_TIM_HANDLE &htim7
+
 /*********ROM COMMANDS ***********/
-#define SEARCH_ROM		0xF0
-#define READ_ROM		0x33
-#define MATCH_ROM		0x55
-#define SKIP_ROM		0xCC
-#define ALARM_SEARCH	0xEC
+#define SEARCH_ROM 0xF0
+#define READ_ROM 0x33
+#define MATCH_ROM 0x55
+#define SKIP_ROM 0xCC
+#define ALARM_SEARCH 0xEC
 
 /*********FUNCTION COMMANDS*******/
-#define CONVERT_T		0x44
+#define CONVERT_T 0x44
 #define WRITE_SCRATCHPAD 0x4E
-#define READ_SCRATCHPAD	0xBE
-#define COPY_SCRATCHPAD	0x48
-#define RECALL_E2		0xB8
-#define READ_POWER		0xB4
+#define READ_SCRATCHPAD 0xBE
+#define COPY_SCRATCHPAD 0x48
+#define RECALL_E2 0xB8
+#define READ_POWER 0xB4
 
 /*********FUNCTIONS**************/
-
-int DS18B20_Initialize();			//returns 0 if successful, 1 if not
-float DS18B20_Read_Temperature();	//return temperature if successful, 0xffff if not
-float DS18B20_Read_Temperature_NB();
-
-
-void DS18B20_Write_Byte(uint8_t byte);
-uint8_t DS18B20_Read_Byte();
+float DS18B20_Read_Temperature();    //return temperature if successful, 0xffff if not
+float DS18B20_Read_Temperature_NB(); //Non blocking version
 
 #endif /* INC_DS18B20_H_ */
